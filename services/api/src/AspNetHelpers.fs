ddfa39f1b60643ea3857eba6823da272c9010a92
Callum Linington <callum.linington@live.co.uk>
2020-05-22 22:18:31 +0100

    refactor(AspNetHelpers): streamline

--- services/api/src/AspNetHelpers.fs
+++ services/api/src/AspNetHelpers.fs
@@ -3,7 +3,6 @@
 open Giraffe
 open System
 open Newtonsoft.Json
-open System.Reflection
 
 type ResponseMessage<'T> = { result: 'T option; error: string option }
 
@@ -13,17 +12,14 @@ type OptionConverter =
     inherit JsonConverter
     new() = { inherit JsonConverter }
     override _.CanConvert(objectType: Type) =
-        let can = objectType.GetGenericTypeDefinition() = typedefof<Option<_>>
-        can
+        objectType.GetGenericTypeDefinition() = typedefof<Option<_>>
     override _.WriteJson(writer: JsonWriter, value, serializer: JsonSerializer) =
         let properties = value.GetType().GetProperties()
         let isSomeProperty = properties |> Array.find (fun prop -> prop.Name = "IsSome")
-        let isSome = isSomeProperty.GetValue(value, [| value |]) :?> bool
-                       
-        match isSome with
+        match isSomeProperty.GetValue(value, [| value |]) :?> bool with
         | true -> 
             let valueProperty = properties |> Array.find(fun prop -> prop.Name = "Value")
-            let valueValue = valueProperty.GetValue(value)
+            let valueValue = valueProperty.GetValue value
             serializer.Serialize(writer, valueValue)
         | false -> 
             writer.WriteNull()
